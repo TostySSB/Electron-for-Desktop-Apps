9466556d867cce4b22a8df8ef8ec08c920738475
Troy Oster <toster1011@me.com>
2020-05-18 13:24:47 -0500

    Section 2: Video 14

---
+++ videoinfo/index.js
@@ -0,0 +1,10 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+app.on('ready', () => {
+  console.log('App is now ready');
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+})
+
