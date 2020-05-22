d176f6e06dc0d9116cec096ae25adee46c42c0b5
Troy Oster <toster1011@me.com>
2020-05-22 15:08:32 -0500

    Section 3: Done

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,10 +1,24 @@
-const electron = require('electron');
+const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
-app.on('ready', () => {
-  console.log('App is now ready');
-  const mainWindow = new BrowserWindow({});
+let mainWindow;
+
+app.on("ready", () => {
+  console.log("App is now ready");
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-})
+});
 
+ipcMain.on("video:submit", (event, path) => {
+  ffmpeg.ffprobe(path, (err, data) => {
+    console.log("video duration is " + data.format.duration);
+    event.reply("video:metadata", data.format.duration);
+  });
+  
+});
