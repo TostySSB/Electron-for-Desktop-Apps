da574d18e3f217d91536765e5469677178e27ab0
Troy Oster <toster1011@me.com>
2020-05-22 16:33:43 -0500

    Section 4: Video 42

---
+++ ElectronCode-master/boilerplates/tasky/index.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const electron = require('electron');
+const { app, BrowserWindow, Tray } = electron;
+
+let mainWindow, tray;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    show: false,
+    resizable: false,
+    frame: false,
+    height: 500,
+    width: 300,
+    webPreferences:{
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
+  tray = new Tray(iconPath)
+  tray.on('click', ()=> {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    }else{
+      mainWindow.show();
+    }
+    
+  })
+});
